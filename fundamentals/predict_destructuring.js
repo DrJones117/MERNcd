12fff2d2b8a72ec981bafc4c65b64059253dbe31
Judah Kahler <judah.kahler@gmail.com>
2023-11-27 16:42:44 -0600

    finished predicting destructuring

--- fundamentals/predict_destructuring.js
+++ fundamentals/predict_destructuring.js
@@ -7,3 +7,45 @@ const [ ,otherRandomCar ] = cars
 console.log(randomCar)
 console.log(otherRandomCar)
 
+// Prediction: Tesla, Mercedes | Result: Tesla, Mercedes
+
+// === 2 ===
+
+const employee = {
+    name: 'Elon',
+    age: 47,
+    company: 'Tesla'
+}
+const { name: otherName } = employee;
+//Predict the output
+console.log(name);
+console.log(otherName);
+
+// Prediction: undefined | Result: name is not defined
+
+// === 3 ===
+
+const person = {
+    name: 'Phil Smith',
+    age: 47,
+    height: '6 feet'
+}
+const password = '12345';
+const { password: hashedPassword } = person;  
+//Predict the output
+console.log(password);
+console.log(hashedPassword);
+
+// Prediction: 12345, undefined | Result: 12345, undefined
+
+// === 4 ===
+
+const numbers = [8, 2, 3, 5, 6, 1, 67, 12, 2];
+const [,first] = numbers;
+const [,,,second] = numbers;
+const [,,,,,,,,third] = numbers;
+//Predict the output
+console.log(first == second);
+console.log(first == third);
+
+// Prediction: false, true | Result: false, true
