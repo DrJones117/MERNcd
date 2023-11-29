be6e6b1c0cabb77cb4471f4c7d405dfbf8254ace
Judah Kahler <judah.kahler@gmail.com>
2023-11-29 12:55:42 -0600

    first_jsx finished

---
+++ intro_to_react/first_jsx/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Hello Dojo!</h1>
+      <ul>
+        <li>Learn React</li>
+        <li>Climb Mt. Everest</li>
+        <li>Run a marathon</li>
+        <li>Feed the Dogs</li>
+      </ul>
+    </>
+  )
+}
+
+export default App
