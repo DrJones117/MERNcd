baf3f8fec012bc736d632b0a9f1cb9390276d284
Judah Kahler <judah.kahler@gmail.com>
2023-11-30 16:42:18 -0600

    More Forms assignment finished.

--- lifting_state/more_forms/src/components/Form.jsx
+++ lifting_state/more_forms/src/components/Form.jsx
@@ -6,22 +6,69 @@ const Form = (props) => {
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
     const [confirmPassword, setConfirmPassword] = useState("")
+    const [hasBeenSubmitted, setHasBeenSubmitted] = useState(false)
 
-    const creatUser = (e) => {
+    const createUser = (e) => {
         e.preventDefault();
 
         const newUser = {firstName, lastName, email, password, confirmPassword}
-        setFirstName(console.log(firstName));
         setLastName("");
         setEmail("");
         setPassword("");
         setConfirmPassword("");
+
+        setHasBeenSubmitted( true );
+    };
+
+    const subMessage = () => {
+        if ( hasBeenSubmitted ) {
+            return "Thanks for submitting the form";
+        } else {
+            return "Welcome, please submit the form."
+        }
+    };
+
+    const fNameMessage = () => {
+        if (firstName.length < 2 && firstName.length > 0) {
+            return "First Name should be at least 2 characters long."
+        }
     }
+
+    const lNameMessage = () => {
+        if (lastName.length < 2 && lastName.length > 0) {
+            return "Last Name should be at least 2 characters long."
+        }
+    }
+
+    const emailMessage = () => {
+        if (email.length < 5 && email.length > 0) {
+            return "Email should be at least 5 characters long."
+        }
+    }
+
+    const passwordMessage = () => {
+        if (password.length < 8 && password.length > 0) {
+            return "Password should be at least 8 characters long."
+        }
+    }
+
+    const confirmMessage = () => {
+        if (password !== confirmPassword && password.length > 0 || confirmPassword.length > 0) {
+            return "Passwords must match."
+        }
+    }
+
 
     return (
         <>
-            <h1>Create a User</h1>
-            <form onSubmit={ creatUser }>
+        <h1>Create User</h1>
+            <form onSubmit={ createUser }>
+                <h3>{ subMessage() }</h3>
+                <p>{ fNameMessage() }</p>
+                <p>{ lNameMessage() }</p>
+                <p>{ emailMessage() }</p>
+                <p>{ passwordMessage() }</p>
+                <p>{ confirmMessage() }</p>
                 <div>
                     <label>First Name: </label>
                     <input id="firstName" type="text" value={firstName} 
@@ -49,7 +96,7 @@ const Form = (props) => {
 
                 <div>
                     <label>Password: </label>
-                    <input id="password" type="text" value={password} onChange = { (e) => {
+                    <input id="password" type="password" value={password} onChange = { (e) => {
                         console.log(e.target.value)
                         setPassword(e.target.value) 
                     } } />
@@ -57,7 +104,7 @@ const Form = (props) => {
 
                 <div>
                     <label>Confirm Password: </label>
-                    <input id="confirmPassword" type="text" value={confirmPassword} onChange = { (e) => {
+                    <input id="confirmPassword" type="password" value={confirmPassword} onChange = { (e) => {
                         console.log(e.target.value)
                         setConfirmPassword(e.target.value) 
                     } } />
@@ -65,6 +112,7 @@ const Form = (props) => {
 
                 <input id="submitBtn" type="submit" value="Create User"/>
             </form>
+            <p>Live data: { firstName } {lastName} {email} {password} {confirmPassword}</p>
         </>
     );
 }
