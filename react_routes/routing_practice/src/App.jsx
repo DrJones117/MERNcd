5e4bb5dea2c76fdee4437effe2f171c66c87ac32
Judah Kahler <judah.kahler@gmail.com>
2023-12-04 16:39:03 -0600

    finished Practice routin assignment

--- react_routes/routing_practice/src/App.jsx
+++ react_routes/routing_practice/src/App.jsx
@@ -2,32 +2,38 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { useParams } from "react-router"; 
+import {
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+
+const Welcome = (props) => {
+  return (
+    <h1>Welcome</h1>
+  );
+}
+
+const OtherRoute = (prop) => {
+  const { par } = useParams();
+  const { C } = useParams();
+  const { bgC } = useParams();
+
+  return (
+    isNaN(par) ? <h1 style={{color: C, backgroundColor: bgC, width: "1300px"}}>The Word is { par }</h1> : <h1>The Number is { par }</h1>
+  )
+}
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <Routes>
+      <Route path="/home" element={<Welcome />}/>
+      <Route path="/:par" element={<OtherRoute />}/>
+      <Route path="/:par/:C/:bgC" element={<OtherRoute />}/>
+    </Routes>
     </>
   )
 }
