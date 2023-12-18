ed89cd26731c2e2fb15092aabe7c8ae4a0d4dfac
Judah Kahler <judah.kahler@gmail.com>
2023-12-18 11:16:17 -0600

    algos incomplete

--- algos_mern/Day1w4.js
+++ algos_mern/Day1w4.js
@@ -14,9 +14,26 @@
 const str1 = "b70a164c32a20c10";
 const expected1 = "a184b70c42";
 
-function rehash(str) {}
+function rehash(str) {
+    const temp = [];
+    let start;
+    let end;
+    for (let i = 0; i < str.length; i++) {
+        for (let j = i + 1; j < str.length; j++) {
+            if (isNaN(str[i]) && isNaN(str[j])) {
+                start = i
+                end = j
+                temp.push(str.slice(start, end))
+                break
+            } 
+            if (isNaN())
+        }
+    }
+    return temp
+}
 
-console.log(rehash(str1) === expected1, "<-- should be \"true\"");
+// console.log(rehash(str1) === expected1, "<-- should be \"true\"");
+console.log(rehash(str1));
 
 // hints
 // isNaN
